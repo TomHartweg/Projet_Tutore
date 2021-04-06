3fe690202dff0c6170fe8540b113a3ac197d8f69
JProulk <ben.pub.gm@gmail.com>
2021-04-06 12:02:23 +0200

    a

--- detection_CMS_MAT/particule.h
+++ detection_CMS_MAT/particule.h
@@ -21,10 +21,11 @@ class particule {
   int ID; //ID de la particule
   std::vector<double> impulsion; //Vecteur quadri-impulsion //est un pointeur vers un quadrivecteur
   double mass; // masse de la particule
+  double norme; //norme de l'impulsion de la particule
+  std::vector<double> directioncar; //vecteur directeur de l'impulsion de la particule en coordonees cartesiennes
  public:
    //constructeurs
    particule();//constucteur par default
-   particule(int, vector<double> ,double); //constructeur a partir de pointeur de quadrivecteur
    particule(int, vector<double> ,double); //constructeur a partir d'entier, vecteur, double
   //mutateurs
   void setID(int n){ID=n;};
@@ -35,6 +36,8 @@ class particule {
   double getmass(){return mass;};
   int getID(){return ID;};
   double gamma();
+  double getnorme(){return norme;};
+  double getdirectioncar(int i){return directioncar[i];};
   //autres methodes
   bool detectCMS(double,double,double);//verifie la desintegration de la particule dans un cylindre selon z de dimension R,H pour un ctau donne
   bool detectMAT(double,double,double,double,double,double,double);//verifie la desintegration de la particule dans un pave de dimension DX DY DZ et position X Y Z pour un ctau donne
