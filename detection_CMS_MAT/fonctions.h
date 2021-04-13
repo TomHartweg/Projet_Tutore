175e3896b37094c1884940fc81a4780638aae8b3
JProulk <ben.pub.gm@gmail.com>
2021-04-13 15:22:20 +0200

    CMS

---
+++ detection_CMS_MAT/fonctions.h
@@ -0,0 +1,21 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+
+#include <iostream> // std::cout, std::fixed
+#include <iomanip> // std::setprecision
+#include <math.h>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <chrono>
+using namespace std;
+
+
+#ifndef FONCTION_H
+#define FONCTION_H
+std::vector<double> car_to_cms(std::vector<double>&);
+
+#endif
+std::vector<double> car_to_cms (std::vector<double>&);
+
+#endif
